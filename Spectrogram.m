d99a789bd6cf9fe18fdc9b741b3bd87f1d7f2307
Lưu Đức Mạnh <44210778+Manhluu97@users.noreply.github.com>
2018-10-21 17:19:08 +0700

    Add files via upload

---
+++ Spectrogram.m
@@ -0,0 +1,8 @@
+win = 128;    %Do dai
+hop = win/2;   
+figure(3);
+nfft = win;
+spectrogram(a,win,hop,nfft,fs,'yaxis');
+yt = get(gca,'YTick'); 
+set(gca,'YTickLabel', sprintf('%.0f|',yt));
+title('Spectrogram');
